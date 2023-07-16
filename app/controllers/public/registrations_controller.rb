4cdc1059e62d90db603990f9a63d94b740839b80
EC2 Default User <yomoyomo0503@gmail.com>
2023-07-16 07:53:16 +0000

    [Add]ユーザー：新規登録・ログイン機能

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -9,6 +9,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def after_sign_up_path_for(resource)
+    customers_my_page_path
+  end
+
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
