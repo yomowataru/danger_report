06244338aef955008a01a92f410eeecbff7a3086
EC2 Default User <yomoyomo0503@gmail.com>
2023-07-17 06:07:44 +0000

    [Add]ユーザー：退会機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,16 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    customer = current_customer
+    customer.update(customer_params)
+    redirect_to customers_my_page_path
   end
 
   def index
@@ -15,5 +20,17 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdraw
+    @customer = current_customer
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email,:name)
   end
 end
