4c9c54aee089b1fe19223577902683556488a486
EC2 Default User <yomoyomo0503@gmail.com>
2023-08-17 13:42:14 +0000

    [Add]共通テンプレートの修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,8 +2,7 @@ class Admin::HomesController < ApplicationController
   def top
     @tags = Tag.all
     @reports = params[:name].present? ? Tag.find(params[:name]).reports.order(created_at: :desc) : Report.all.order(created_at: :desc)
-    # ↓共通テンプレート用にパスを変数化
-    @link = admin_report_path(@reports.ids)
+
   end
 
   def index
